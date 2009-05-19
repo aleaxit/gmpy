b682eaaf5e3ff4af9e4116d3a9e4a6bd673d010f
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2009-05-19 04:54:23 +0000

    Changes to test to related to removal of automatic coercion.  8 tests are known to still fail.

--- trunk/test/gmpy_test.py
+++ trunk/test/gmpy_test.py
@@ -8,9 +8,6 @@ import sys
 import doctest
 import gmpy
 
-def writeln(s):
-    sys.stdout.write(s+'\n')
-
 if sys.argv[-1] == 'debug':
     gmpy.set_debug(1)
 
@@ -26,19 +23,19 @@ test_modules = (gmpy_test_cvr, gmpy_test_rnd, gmpy_test_mpf,
     gmpy_test_mpq, gmpy_test_mpz, gmpy_test_dec)
 
 _g = gmpy
-writeln("Unit tests for gmpy 1.05")
-writeln("    on Python %s" % sys.version)
-writeln("Testing gmpy %s (GMP %s), default caching (%s, %s, %s..%s)" % (
+print "Unit tests for gmpy 1.05"
+print "    on Python %s" % sys.version
+print "Testing gmpy %s (GMP %s), default caching (%s, %s, %s..%s)" % (
     (_g.version(), _g.gmp_version(), _g.get_zcache(), _g.get_qcache(),
-            ) + _g.get_zconst()))
+            ) + _g.get_zconst())
 
 pf, pt = 0, 0
 for x in test_modules:
     testit = x._test()
     failures, tests = testit
     if tests == 0: continue
-    writeln(x.__name__,)
-    writeln("%3d tests, %d failures" % (tests-pt, failures-pf))
+    print x.__name__,
+    print "%3d tests, %d failures" % (tests-pt, failures-pf)
     pf, pt = failures, tests
 
 doctest.master.summarize(1)
