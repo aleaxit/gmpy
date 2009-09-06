b9e7884280cac51b80da62876b0b15843dcfef5d
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2009-09-06 04:26:35 +0000

    Added new helper functions for mpmath: _mpmath_trim and _mpmath_mult.

--- trunk/src/gmpy_mpmath.c
+++ trunk/src/gmpy_mpmath.c
@@ -312,3 +312,220 @@ Pympz_mpmath_create(PyObject *self, PyObject *args)
     return mpmath_build_mpf(sign, upper, newexp2, bc);
 }
 
+/* Second version of helper functions for mpmath. See Issue 33 for details.
+ */
+
+static PyObject *
+do_mpmath_trim(mpz_t xman, mpz_t xexp, long prec, char rnd) {
+    PyObject *result = 0;
+    PympzObject *rman = 0, *rexp = 0;
+    long bc, shift, zbits, carry = 0;
+    mpz_t lower;
+
+    result = PyTuple_New(2);
+    rman = Pympz_new();
+    rexp = Pympz_new();
+    if(!result || !rman || !rexp) {
+        Py_XDECREF(result);
+        Py_XDECREF((PyObject*)rman);
+        Py_XDECREF((PyObject*)rexp);
+        return NULL;
+    }
+    mpz_set(rman->z, xman);
+    mpz_set(rexp->z, xexp);
+
+    /* If the mantissa is 0, just return the canonical representation of 0. */
+    if(!mpz_sgn(rman->z)) {
+        fprintf(stderr, "zero\n");
+        mpz_set_ui(rexp->z, 0);
+        goto return_result;
+    }
+
+    /* Remove trailing 0 bits and adjust exponenet. */
+    if((zbits = mpz_scan1(rman->z, 0))) {
+        mpz_tdiv_q_2exp(rman->z, rman->z, zbits);
+        mpz_add_ui(rexp->z, rexp->z, zbits);
+    }
+
+    /* If prec is 0, return with trailing 0 bits removed. */
+    if(prec == 0) goto return_result;
+
+    bc = mpz_sizeinbase(rman->z, 2);
+
+    /* If bc <= prec, just return. */
+    if(bc <= prec) goto return_result;
+
+    /* We need to round the mantissa. */
+    shift = bc - prec;
+    switch(rnd) {
+        case 'f':
+            mpz_fdiv_q_2exp(rman->z, rman->z, shift);
+            break;
+        case 'c':
+            mpz_cdiv_q_2exp(rman->z, rman->z, shift);
+            break;
+        case 'd':
+            if(mpz_sgn(rman->z) > 0) {
+                mpz_fdiv_q_2exp(rman->z, rman->z, shift);
+            } else {
+                mpz_cdiv_q_2exp(rman->z, rman->z, shift);
+            }
+            break;
+        case 'u':
+            if(mpz_sgn(rman->z) > 0) {
+                mpz_cdiv_q_2exp(rman->z, rman->z, shift);
+            } else {
+                mpz_fdiv_q_2exp(rman->z, rman->z, shift);
+            }
+            break;
+        case 'n':
+        default:
+            mpz_inoc(lower);
+            mpz_tdiv_r_2exp(lower, rman->z, shift);
+            mpz_tdiv_q_2exp(rman->z, rman->z, shift);
+            /* lower is not 0 so it must have at least 1 bit set */
+            if(mpz_sizeinbase(lower, 2) == shift) {
+                /* lower is >= 1/2 */
+                if(mpz_scan1(lower, 0) == shift-1) {
+                    /* lower is exactly 1/2 */
+                    if(mpz_odd_p(rman->z))
+                        carry = 1;
+                } else {
+                    carry = 1;
+                }
+            }
+            mpz_cloc(lower);
+            /* Add the carry bit and readjust for trailing 0 bits. */
+            if(carry) {
+                mpz_add_ui(rman->z, rman->z, 1);
+                zbits = mpz_scan1(rman->z, 0);
+                mpz_tdiv_q_2exp(rman->z, rman->z, zbits);
+                mpz_add_ui(rexp->z, rexp->z, zbits);
+            }
+    }
+    mpz_add_ui(rexp->z, rexp->z, shift);
+
+return_result:
+    PyTuple_SET_ITEM(result, 0, (PyObject*)rman);
+    PyTuple_SET_ITEM(result, 1, (PyObject*)rexp);
+    return result;
+}
+
+static char doc_mpmath_trimg[]="\
+_mpmath_trim(xman, xexp, prec, rounding):\n\
+    Return (man, exp) by rounding xman*(2**xexp) to prec bits using the\n\
+    specified rounding mode.\n\
+";
+static PyObject *
+Pympz_mpmath_trim(PyObject *self, PyObject *args)
+{
+    PyObject *arg0 = 0, *arg1 = 0, *result;
+    long prec = 0;
+    const char *rnd = "d";
+
+    switch(PyTuple_GET_SIZE(args)) {
+        case 4:
+            rnd = Py2or3String_AsString(PyTuple_GET_ITEM(args, 3));
+        case 3:
+            prec = Py2or3Int_AsLong(PyTuple_GET_ITEM(args, 2));
+        case 2:
+            arg1 = (PyObject*)anyint2Pympz(PyTuple_GET_ITEM(args, 1));
+        case 1:
+            arg0 = (PyObject*)anyint2Pympz(PyTuple_GET_ITEM(args, 0));
+    }
+    if(!arg0 || !arg1 || PyErr_Occurred()) {
+        PyErr_SetString(PyExc_TypeError, "arguments mpz, mpz, long, char needed");
+        Py_XDECREF(arg0);
+        Py_XDECREF(arg1);
+        return NULL;
+    } else {
+        result = do_mpmath_trim(Pympz_AS_MPZ(arg0), Pympz_AS_MPZ(arg1), prec, rnd[0]);
+        Py_DECREF(arg0);
+        Py_DECREF(arg1);
+        return result;
+    }
+}
+
+static char doc_mpmath_addg[]="\
+_mpmath_add(xman, xexp, yman, yexp, prec, rounding):\n\
+    Return (man, exp) by rounding xman*2**xexp + yman*2**yexp to prec\n\
+    bits using the specified rounding mode.\n\
+";
+static PyObject *
+Pympz_mpmath_add(PyObject *self, PyObject *args)
+{
+    return Py2or3Int_FromLong(0);
+}
+
+static char doc_mpmath_multg[]="\
+_mpmath_mult(xman, xexp, yman, yexp, prec, rounding):\n\
+    Return (man, exp) by rounding xman*2**xexp * yman*2**yexp to prec\n\
+    bits using the specified rounding mode.\n\
+";
+static PyObject *
+Pympz_mpmath_mult(PyObject *self, PyObject *args)
+{
+    PyObject *arg0 = 0, *arg1 = 0, *arg2 = 0, * arg3 = 0, *result;
+    mpz_t man, exp;
+    long prec = 0;
+    const char *rnd = "d";
+
+    switch(PyTuple_GET_SIZE(args)) {
+        case 6:
+            rnd = Py2or3String_AsString(PyTuple_GET_ITEM(args, 5));
+        case 5:
+            prec = Py2or3Int_AsLong(PyTuple_GET_ITEM(args, 4));
+        case 4:
+            arg3 = (PyObject*)anyint2Pympz(PyTuple_GET_ITEM(args, 3));
+        case 3:
+            arg2 = (PyObject*)anyint2Pympz(PyTuple_GET_ITEM(args, 2));
+        case 2:
+            arg1 = (PyObject*)anyint2Pympz(PyTuple_GET_ITEM(args, 1));
+        case 1:
+            arg0 = (PyObject*)anyint2Pympz(PyTuple_GET_ITEM(args, 0));
+    }
+    if(!arg0 || !arg1 || !arg2 || !arg3 || PyErr_Occurred()) {
+        PyErr_SetString(PyExc_TypeError, "arguments mpz, mpz, mpz, mpz, long, char needed");
+        Py_XDECREF(arg0);
+        Py_XDECREF(arg1);
+        Py_XDECREF(arg2);
+        Py_XDECREF(arg3);
+        return NULL;
+    } else {
+        mpz_inoc(man);
+        mpz_inoc(exp);
+        mpz_mul(man, Pympz_AS_MPZ(arg0), Pympz_AS_MPZ(arg2));
+        mpz_add(exp, Pympz_AS_MPZ(arg1), Pympz_AS_MPZ(arg3));
+        result = do_mpmath_trim(man, exp, prec, rnd[0]);
+        mpz_cloc(man);
+        mpz_cloc(exp);
+        Py_DECREF(arg0);
+        Py_DECREF(arg1);
+        Py_DECREF(arg2);
+        Py_DECREF(arg3);
+        return result;
+    }
+}
+
+static char doc_mpmath_divg[]="\
+_mpmath_div(xman, xexp, yman, yexp, prec, rounding):\n\
+    Return (man, exp) by rounding xman*2**xexp / yman*2**yexp to prec\n\
+    bits using the specified rounding mode.\n\
+";
+static PyObject *
+Pympz_mpmath_div(PyObject *self, PyObject *args)
+{
+    return Py2or3Int_FromLong(0);
+}
+
+static char doc_mpmath_sqrtg[]="\
+_mpmath_sqrt(man, exp, prec, rounding):\n\
+    Return (man, exp) by rounding square_root(xman*2**xexp)) to prec\n\
+    bits using the specified rounding mode.\n\
+";
+static PyObject *
+Pympz_mpmath_sqrt(PyObject *self, PyObject *args)
+{
+    return Py2or3Int_FromLong(0);
+}
+
