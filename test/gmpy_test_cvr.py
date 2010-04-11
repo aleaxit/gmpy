3d072f2d0d39a47cfdae16e4df6912142a86c866
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2010-04-11 04:59:30 +0000

    [trunk] Support MPIR 2.0.

--- test/gmpy_test_cvr.py
+++ test/gmpy_test_cvr.py
@@ -2,9 +2,9 @@
 # relies on Tim Peters' "doctest.py" test-driver
 # test-version 1.20
 r'''
->>> print int(_g.gmp_version()[:3] in ('5.0', '4.3', '4.2', '4.1', '4.0', ''))
+>>> print int(_g.gmp_version()[:3] in ('5.0', '4.3', '4.2', ''))
 1
->>> print int(_g.mpir_version()[:3] in ('', '0.9', '1.0', '1.1', '1.2', '1.3'))
+>>> print int(_g.mpir_version()[:3] in ('', '0.9', '1.0', '1.1', '1.2', '1.3', '2.0'))
 1
 >>> _g.version()
 '1.20'
--- test3/gmpy_test_cvr.py
+++ test3/gmpy_test_cvr.py
@@ -2,9 +2,9 @@
 # relies on Tim Peters' "doctest.py" test-driver
 # test-version 1.20
 r'''
->>> _g.gmp_version()[:3] in ('5.0', '4.3', '4.2', '4.1', '4.0', '3.1', '')
+>>> _g.gmp_version()[:3] in ('5.0', '4.3', '4.2', '')
 True
->>> _g.mpir_version()[:3] in ('0.9', '1.0', '1.1', '1.2', '1.3', '')
+>>> _g.mpir_version()[:3] in ('0.9', '1.0', '1.1', '1.2', '1.3', '2.0', '')
 True
 >>> _g.version()
 '1.20'
