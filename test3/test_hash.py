ff6822ffe9c89aa5440bf7d3bf6242a4692d6139
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2010-05-23 23:52:51 +0000

    [gmpy_1_12] Improved error message for test_hash.py

--- test3/test_hash.py
+++ test3/test_hash.py
@@ -24,9 +24,9 @@ for d in [1, -2, 3, -47, m, m*2]:
             for k in [-1, 1, 7, 11, -(2**15), 2**16, 2**30, 2**31, 2**32, 2**33, 2**61, -(2**62), 2**63, 2**64]:
                 val = k*(s + i)
                 if val:
-                    assert hash(fractions.Fraction(d,val)) == hash(gmpy.mpq(d,val))
+                    assert hash(fractions.Fraction(d,val)) == hash(gmpy.mpq(d,val)), (d,val,hash(fractions.Fraction(d,val)),hash(gmpy.mpq(d,val)))
                 if d:
-                    assert hash(fractions.Fraction(val,d)) == hash(gmpy.mpq(val,d))
+                    assert hash(fractions.Fraction(val,d)) == hash(gmpy.mpq(val,d)), (val,d,hash(fractions.Fraction(val,d)),hash(gmpy.mpq(val,d)))
 
 
 print("hash tests for rational values passed")
