43ac66825b69448cc4d5608920577c70ae3d0072
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2010-05-23 23:23:25 +0000

    [trunk] Fixed memory leak in hash(mpq). Improved error message for test_hash.py

--- test3/test_hash.py
+++ test3/test_hash.py
@@ -1,6 +1,6 @@
 # Hash test for Python 3.2.
 
-import gmpy
+import gmpy2
 import fractions
 
 import sys
@@ -14,7 +14,7 @@ for s in [0, m//2, m, m*2, m*m]:
     for i in range(-10,10):
         for k in [-1, 1, 7, 11, -(2**15), 2**16, 2**30, 2**31, 2**32, 2**33, 2**61, -(2**62), 2**63, 2**64]:
             val = k*(s + i)
-            assert hash(val) == hash(gmpy.mpz(val))
+            assert hash(val) == hash(gmpy2.mpz(val))
 
 print("hash tests for integer values passed")
 
@@ -24,9 +24,9 @@ for d in [1, -2, 3, -47, m, m*2]:
             for k in [-1, 1, 7, 11, -(2**15), 2**16, 2**30, 2**31, 2**32, 2**33, 2**61, -(2**62), 2**63, 2**64]:
                 val = k*(s + i)
                 if val:
-                    assert hash(fractions.Fraction(d,val)) == hash(gmpy.mpq(d,val))
+                    assert hash(fractions.Fraction(d,val)) == hash(gmpy2.mpq(d,val)), (d,val,hash(fractions.Fraction(d,val)),hash(gmpy.mpq(d,val)))
                 if d:
-                    assert hash(fractions.Fraction(val,d)) == hash(gmpy.mpq(val,d))
+                    assert hash(fractions.Fraction(val,d)) == hash(gmpy2.mpq(val,d)), (val,d,hash(fractions.Fraction(val,d)),hash(gmpy.mpq(val,d)))
 
 
 print("hash tests for rational values passed")
