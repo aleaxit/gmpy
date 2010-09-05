300a071f641dab41d13a2a56cc789f4107796ee1
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2010-09-05 21:01:39 +0000

    [trunk] Clean up doc-strings, part 3.

--- src/gmpy_misc.c
+++ src/gmpy_misc.c
@@ -12,72 +12,61 @@
 __MPIR_VERSION * 10000 + \
 __MPIR_VERSION_MINOR * 100 + \
 __MPIR_VERSION_PATCHLEVEL
-#if ((MPIR_VER < 10300) && (MPIR_VER > 10399))
-char gmpy_license[] = "\
-The GMPY2 source code is licensed under LGPL 2.1 or later. \
-This version of the MPIR library is licensed under LGPL 3 or later. \
-Therefore, this combined module is licensed under LGPL 3 or later.\
-";
-#else
-char gmpy_license[] = "\
-The GMPY2 source code is licensed under LGPL 2.1 or later. \
-This version of the MPIR library is licensed under LGPL 2.1 or later. \
-Therefore, this combined module is licensed under LGPL 2.1 or later.\
-";
-#endif
-#else
-#define GNU_MP_VER \
-__GNU_MP_VERSION * 10000 + \
-__GNU_MP_VERSION_MINOR * 100 + \
-__GNU_MP_VERSION_PATCHLEVEL
-#if GNU_MP_VER > 40201
-char gmpy_license[] = "\
-The GMPY2 source code is licensed under LGPL 2.1 or later. \
-This version of the GMP library is licensed under LGPL 3 or later. \
-Therefore, this combined module is licensed under LGPL 3 or later.\
-";
-#else
-char gmpy_license[] = "\
-The GMPY2 source code is licensed under LGPL 2.1 or later. \
-This version of the GMP library is licensed under LGPL 2.1 or later. \
-Therefore, this combined module is licensed under LGPL 2.1 or later.\
-";
-#endif
-#endif
-#undef GNU_MP_VER
 
-static char doc_license[]="\
-license(): returns string giving license information\n\
-";
+#if ((MPIR_VER < 10300) && (MPIR_VER > 10399))
+static char gmpy_license[] = "\
+The GMPY2 source code is licensed under LGPL 2.1 or later. This version \n\
+of the MPIR library is licensed under LGPL 3 or later. Therefore, this \n\
+combined module is licensed under LGPL 3 or later.";
+#else
+static char gmpy_license[] = "\
+The GMPY2 source code is licensed under LGPL 2.1 or later. This version \n\
+of the MPIR library is licensed under LGPL 2.1 or later. Therefore, this \n\
+combined module is licensed under LGPL 2.1 or later.";
+#endif
+#else
+char gmpy_license[] = "\
+The GMPY2 source code is licensed under LGPL 2.1 or later. The GMP \n\
+library is licensed under LGPL 3 or later. Therefore, this combined \n\
+module is licensed under LGPL 3 or later.";
+#endif
+#undef MPIR_VER
+
+PyDoc_STRVAR(doc_license,
+"license() -> string\n\n\
+Return string giving license information.");
+
 static PyObject *
 Pygmpy_get_license(PyObject *self, PyObject *args)
 {
     return Py_BuildValue("s", gmpy_license);
 }
 
-/* return GMPY, resp. GMP, versions, or CVS Id, as strings */
-static char doc_version[]="\
-version(): returns string giving current GMPY version\n\
-";
+PyDoc_STRVAR(doc_version,
+"version() -> string\n\n\
+Return string giving current GMPY2 version.");
+
 static PyObject *
 Pygmpy_get_version(PyObject *self, PyObject *args)
 {
     return Py_BuildValue("s", gmpy_version);
 }
 
-static char doc_cvsid[]="\
-_cvsid(): returns string giving current GMPY cvs Id\n\
-";
+PyDoc_STRVAR(doc_cvsid,
+"_cvsid() -> string\n\n\
+Return string giving current GMPY2 cvs Id.");
+
 static PyObject *
 Pygmpy_get_cvsid(PyObject *self, PyObject *args)
 {
     return Py_BuildValue("s", _gmpy_cvs);
 }
 
-static char doc_gmp_version[]="\
-gmp_version(): returns string giving current GMP version. Empty\n\
-string returned if MPIR was used.\n\
-";
+PyDoc_STRVAR(doc_gmp_version,
+"gmp_version() -> string\n\n\
+Return string giving current GMP version. Empty string returned if\n\
+MPIR is used.");
+
 static PyObject *
 Pygmpy_get_gmp_version(PyObject *self, PyObject *args)
 {
@@ -88,10 +77,11 @@ Pygmpy_get_gmp_version(PyObject *self, PyObject *args)
 #endif
 }
 
-static char doc_mpir_version[]="\
-mpir_version(): returns string giving current MPIR version. Empty\n\
-string returned if GMP was used.\n\
-";
+PyDoc_STRVAR(doc_mpir_version,
+"mpir_version() -> string\n\n\
+Return string giving current MPIR version. Empty string is returned if\n\
+GMP was used.");
+
 static PyObject *
 Pygmpy_get_mpir_version(PyObject *self, PyObject *args)
 {
@@ -102,9 +92,10 @@ Pygmpy_get_mpir_version(PyObject *self, PyObject *args)
 #endif
 }
 
-static char doc_gmp_limbsize[]="\
-gmp_limbsize(): returns the number of bits per limb\n\
-";
+PyDoc_STRVAR(doc_gmp_limbsize,
+"gmp_limbsize() -> integer\n\n\
+Return the number of bits per limb.");
+
 static PyObject *
 Pygmpy_get_gmp_limbsize(PyObject *self, PyObject *args)
 {
@@ -114,36 +105,40 @@ Pygmpy_get_gmp_limbsize(PyObject *self, PyObject *args)
 /*
  * access cache options
  */
-static char doc_get_cache[]="\
-get_cache(): returns the current cache-size (number of objects)\n\
-and maximum size per object (number of limbs) for all objects.\n\
-";
+
+PyDoc_STRVAR(doc_get_cache,
+"get_cache() -> (cache_size, object_size)\n\n\
+Return the current cache size (number of objects) and maximum size\n\
+per object (number of limbs) for all GMPY2 objects.");
+
 static PyObject *
 Pygmpy_get_cache(PyObject *self, PyObject *args)
 {
     return Py_BuildValue("ii", options.cache_size, options.cache_obsize);
 }
 
-static char doc_set_cache[]="\
-set_cache(n,size): sets the current cache-size (number of objects) to\n\
-'n' and the maximum size per object (number of limbs) to 'size'.\n\
-Note: cache size 'n' must be between 0 and 1000, included. Object size\n\
-'size' must be between 0 and 16384, included.\n\
-";
+PyDoc_STRVAR(doc_set_cache,
+"set_cache(cache_size, object_size)\n\n\
+Set the current cache size (number of objects) and the maximum size\n\
+per object (number of limbs). Raises ValueError if cache size exceeds\n\
+1000 or object size exceeds 16384.");
+
 static PyObject *
 Pygmpy_set_cache(PyObject *self, PyObject *args)
 {
     int newcache, newsize;
-    if(!PyArg_ParseTuple(args, "ii", &newcache, &newsize))
+
+    if (!PyArg_ParseTuple(args, "ii", &newcache, &newsize))
         return NULL;
-    if(newcache<0 || newcache>MAX_CACHE) {
+    if (newcache<0 || newcache>MAX_CACHE) {
         VALUE_ERROR("cache size must between 0 and 1000");
         return NULL;
     }
-    if(newsize<0 || newsize>MAX_CACHE_LIMBS) {
+    if (newsize<0 || newsize>MAX_CACHE_LIMBS) {
         VALUE_ERROR("object size must between 0 and 16384");
         return NULL;
     }
+
     options.cache_size = newcache;
     options.cache_obsize = newsize;
     set_zcache();
@@ -151,31 +146,32 @@ Pygmpy_set_cache(PyObject *self, PyObject *args)
     set_pympzcache();
     set_pympqcache();
     set_pyxmpzcache();
-    return Py_BuildValue("");
+    Py_RETURN_NONE;
 }
 
-/* set a module-global flag, return previously-set value */
-static char doc_set_debug[]="\
-set_debug(n): resets (if n==0) or sets (if n!=0) the module\n\
-level 'debug' setting, giving detailed info to stderr; also\n\
-returns the previous value of this module-level setting.\n\
-Note: only useful to debug gmpy's own internals!\n\
-";
+PyDoc_STRVAR(doc_set_debug,
+"set_debug(boolean) -> boolean\n\n\
+Set (if True) or clear (if False) the module level 'debug' setting\n\
+and returns the previous value. If set, diagnostic information is\n\
+sent to stderr. Note: only useful to debug GMPY2's own internals!");
+
 static PyObject *
 Pygmpy_set_debug(PyObject *self, PyObject *args)
 {
     long old = options.debug;
 
-    if(!PyArg_ParseTuple(args, "l", &options.debug))
+    if (!PyArg_ParseTuple(args, "l", &options.debug))
         return NULL;
     return Py_BuildValue("l", old);
 }
 
-static char doc_set_prefer_mutable[]="\
-set_prefer_mutable(n): If set, the result of combining a\n\
-mutable and immutable type will be a mutable type. If clear,\n\
-the result will be an immutable type.\n\
-";
+PyDoc_STRVAR(doc_set_prefer_mutable,
+"set_prefer_mutable(boolean) -> boolean\n\n\
+If set to True, the result type of an ambiguous operation (i.e.\n\
+adding an 'mpz' and 'xmpz', or creating a GMPY2 type from a Python\n\
+number) will be an 'xmpz'. Returns the previous value. Default is\n\
+is False.");
+
 static PyObject *
 Pygmpy_set_prefer_mutable(PyObject *self, PyObject *args)
 {
@@ -188,7 +184,7 @@ Pygmpy_set_prefer_mutable(PyObject *self, PyObject *args)
 
 static char doc_set_tagoff[]="\
 set_tagoff(n): resets (if n==0) or sets (if n!=0) the module\n\
-level 'tagoff' setting, removing the 'gmpy.' prefix of the tag\n\
+level 'tagoff' setting, removing the 'gmpy2.' prefix of the tag\n\
 strings used by repr and (optionally) digits/fdigits/qdigits;\n\
 also returns the previous value of this module-level setting.\n\
 ";
@@ -282,48 +278,49 @@ Pygmpy_set_fcoform(PyObject *self, PyObject *args)
 }
 
 /* create a copy of a gmpy2 object */
-static char doc_copym[]="\
-x._copy(): returns a copy of x.\n\
-";
-static char doc_copyg[]="\
-_copy(x): returns a copy of x, x must be a gmpy2 object.\n\
-";
+PyDoc_STRVAR(doc_copym,
+"x._copy() -> gmpy2_object\n\n"
+"Return a copy of x.\n");
+PyDoc_STRVAR(doc_copyg,
+"_copy(x): -> gmpy2_object\n\n"
+"Return a copy of x. Raises TypeError if x is not a gmpy2 object.");
 static PyObject *
 Pympany_copy(PyObject *self, PyObject *other)
 {
-    if(self && Pympz_Check(self))
+    if (self && Pympz_Check(self))
         return (PyObject*)Pympz2Pympz(self);
-    else if(self && Pyxmpz_Check(self))
+    else if (self && Pyxmpz_Check(self))
         return (PyObject*)Pyxmpz2Pyxmpz(self);
-    else if(self && Pympq_Check(self))
+    else if (self && Pympq_Check(self))
         return (PyObject*)Pympq2Pympq(self);
-    else if(self && Pympf_Check(self))
+    else if (self && Pympf_Check(self))
         return (PyObject*)Pympf2Pympf(self, 0);
-    else if(Pympz_Check(other))
+    else if (Pympz_Check(other))
         return (PyObject*)Pympz2Pympz(other);
-    else if(Pyxmpz_Check(other))
+    else if (Pyxmpz_Check(other))
         return (PyObject*)Pyxmpz2Pyxmpz(other);
-    else if(Pympq_Check(other))
+    else if (Pympq_Check(other))
         return (PyObject*)Pympq2Pympq(other);
-    else if(Pympf_Check(other))
+    else if (Pympf_Check(other))
         return (PyObject*)Pympf2Pympf(other, 0);
-    TYPE_ERROR("copy() requires a gmpy2 object as argument");
+    TYPE_ERROR("_copy() requires a gmpy2 object as argument");
     return NULL;
 }
 
-/* produce portable binary form for a gmpy2 object */
-static char doc_binarym[]="\
-x.binary(): returns a Python string (or bytes for Python 3+)\n\
-that is a portable binary representation of a gmpy2 object x.\n\
-The string/bytes can later be passed to the appropriate\n\
-constructor function to obtain an exact copy of x's value.\n\
-";
-static char doc_binaryg[]="\
-binary(x): returns a Python string (or bytes for Python 3+)\n\
-that is a portable binary representation of a gmpy2 object x.\n\
-The string/bytes can later be passed to the appropriate\n\
-constructor function to obtain an exact copy of x's value.\n\
-";
+PyDoc_STRVAR(doc_binarym,
+"x.binary() -> binary string\n\n"
+"Return a Python string (or bytes for Python 3+) that is a portable\n"
+"binary representation of a gmpy2 object x. The binary string can\n"
+"later be passed to the appropriate constructor function to obtain\n"
+"an exact copy of x's value.");
+PyDoc_STRVAR(doc_binaryg,
+"binary(x) -> binary string\n\n"
+"Return a Python string (or bytes for Python 3+) that is a portable\n"
+"binary representation of a gmpy2 object x. The binary string can\n"
+"later be passed to the appropriate constructor function to obtain\n"
+"an exact copy of x's value. Raises TypeError if x is not a gmpy2\n"
+"object.");
+
 static PyObject *
 Pympany_binary(PyObject *self, PyObject *other)
 {
