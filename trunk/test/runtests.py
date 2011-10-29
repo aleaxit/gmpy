41a7baa28c7e09422604c4bc714a9f0873b67c62
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2011-10-29 22:32:25 +0000

    [trunk] Start writing tests in the new format.

--- trunk/test/runtests.py
+++ trunk/test/runtests.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import sys
+import doctest
 import gmpy2
 
 print()
@@ -11,4 +12,19 @@ print("  Complex library:              {0}".format(gmpy2.mpc_version()))
 print("  Caching Values: (Number)      {0}".format(gmpy2.get_cache()[0]))
 print("  Caching Values: (Size, limbs) {0}".format(gmpy2.get_cache()[1]))
 print()
-print("No tests found!")
+
+doctests = ["test_misc.txt"]
+failed = 0
+attempted = 0
+
+for test in doctests:
+    print("Running test: {0}".format(test))
+    result = doctest.testfile(test)
+    print("  Results - Attempted: {1:4d} Failed: {0:4d}".format(*result))
+    failed += result[0]
+    attempted += result[1]
+
+print()
+print("Summary of all tests")
+print("  Summary - Attempted: {0:4d} Failed: {1:4d}".format(attempted, failed))
+
