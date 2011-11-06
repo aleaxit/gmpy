d3cc04ae07aeb2806283e432e75ede3f377ab9ad
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2011-11-06 23:48:30 +0000

    [trunk] More updates to new test suite.

--- trunk/test/runtests.py
+++ trunk/test/runtests.py
@@ -14,7 +14,9 @@ print("  Caching Values: (Size, limbs) {0}".format(gmpy2.get_cache()[1]))
 print()
 
 # The following tests should pass on all supported versions of Python.
-all_doctests = ["test_misc.txt", "test_dec.txt", "test_mpz.txt"]
+all_doctests = ["test_misc.txt", "test_dec.txt", "test_mpz.txt",
+                "test_mpz_comp.txt", "test_mpz_functions.txt",
+                "test_mpz_io.txt"]
 
 # The following tests will only pass on Python 3.2+.
 py32_doctests = ["test_hash.txt"]
@@ -24,7 +26,7 @@ attempted = 0
 
 for test in all_doctests:
     print("Running test: {0}".format(test))
-    result = doctest.testfile(test)
+    result = doctest.testfile(test, globs=globals())
     print("  Results - Attempted: {1:4d} Failed: {0:4d}".format(*result))
     failed += result[0]
     attempted += result[1]
@@ -32,7 +34,7 @@ for test in all_doctests:
 if sys.version_info >= (3,2):
     for test in py32_doctests:
         print("Running test: {0}".format(test))
-        result = doctest.testfile(test)
+        result = doctest.testfile(test, globs=globals())
         print("  Results - Attempted: {1:4d} Failed: {0:4d}".format(*result))
         failed += result[0]
         attempted += result[1]
