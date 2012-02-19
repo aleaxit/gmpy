02b418270eea6afeab31e927971860483622da30
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2012-02-19 23:13:47 +0000

    [trunk] Change license of GMPY2 to LGPL 3+.

--- src/gmpy_mpz.c
+++ src/gmpy_mpz.c
@@ -3,23 +3,26 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Python interface to the GMP or MPIR, MPFR, and MPC multiple precision   *
  * libraries.                                                              *
- *      Copyright 2000 - 2009 Alex Martelli                                *
- *      Copyright 2008, 2009, 2010, 2011, 2012 Case Van Horsen             *
  *                                                                         *
- * This library is free software; you can redistribute it and/or modify it *
- * under the terms of the GNU Lesser General Public License as published   *
- * by the Free Software Foundation; either version 2.1 of the License, or  *
- * (at your option) any later version.                                     *
+ * Copyright 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,               *
+ *           2008, 2009 Alex Martelli                                      *
  *                                                                         *
- * This library is distributed in the hope that it will be useful,         *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       *
- * Lesser General Public License for more details.                         *
+ * Copyright 2008, 2009, 2010, 2011, 2012 Case Van Horsen                  *
+ *                                                                         *
+ * This file is part of GMPY2.                                             *
+ *                                                                         *
+ * GMPY2 is free software: you can redistribute it and/or modify it under  *
+ * the terms of the GNU Lesser General Public License as published by the  *
+ * Free Software Foundation, either version 3 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * GMPY2 is distributed in the hope that it will be useful, but WITHOUT    *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public    *
+ * License for more details.                                               *
  *                                                                         *
  * You should have received a copy of the GNU Lesser General Public        *
- * License along with this library; if not, write to the Free Software     *
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA           *
- * 02110-1301  USA                                                         *
+ * License along with GMPY2; if not, see <http://www.gnu.org/licenses/>    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 static char doc_mpz[] = "\
