43b13d20237aaa60da19ee040ccb4033e76df4ed
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2012-09-29 06:22:56 +0000

    [gmpy 1.16] Fix compatibility with Python 3.3.

--- tags/gmpy_1_16/test3/gmpy_test_dec.py
+++ tags/gmpy_1_16/test3/gmpy_test_dec.py
@@ -3,8 +3,9 @@
 #       or better to allow decimal/most-anything-else interoperability!-)
 # relies on Tim Peters' "doctest.py" test-driver
 
+# Hack to remove the '__dir__' attribute introduced in Python 3.3
 r'''
->>> dir(f)
+>>> list([x for x in dir(f) if x != '__dir__'])
 ['__abs__', '__add__', '__bool__', '__class__', '__delattr__', '__divmod__', '__doc__', '__eq__', '__float__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__int__', '__le__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__pos__', '__pow__', '__radd__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rmod__', '__rmul__', '__rpow__', '__rsub__', '__rtruediv__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '_copy', 'binary', 'ceil', 'digits', 'f2q', 'floor', 'getprec', 'getrprec', 'qdiv', 'reldiff', 'round', 'setprec', 'sign', 'sqrt', 'trunc']
 >>>
 '''
