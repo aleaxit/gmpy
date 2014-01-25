8002d9a2b49eaac928446a0413f604f70729408a
casevh <casevh@f8dd95ac-dd29-0410-980a-fda5e6fcbee1>
2014-01-25 20:16:40 +0000

    [devel] Commit work in progress. Change copyright dates to include 2014.

--- src/gmpy_args.h
+++ src/gmpy_args.h
@@ -6,7 +6,7 @@
  * Copyright 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,               *
  *           2008, 2009 Alex Martelli                                      *
  *                                                                         *
- * Copyright 2008, 2009, 2010, 2011, 2012, 2013 Case Van Horsen            *
+ * Copyright 2008, 2009, 2010, 2011, 2012, 2013, 2014 Case Van Horsen      *
  *                                                                         *
  * This file is part of GMPY2.                                             *
  *                                                                         *
