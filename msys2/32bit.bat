7ac1927364830ca06ea4b413d4cc112eecebff84
Case Van Horsen <casevh@gmail.com>
2017-04-06 21:01:13 -0700

    WIP: msys2 build documentation.

---
+++ msys2/32bit.bat
@@ -0,0 +1,7 @@
+@echo off
+ doskey py26=C:\32\Python26\python.exe $*
+ doskey py27=C:\32\Python27\python.exe $*
+ doskey py34=C:\32\Python34\python.exe $*
+ doskey py35=C:\32\Python35\python.exe $*
+ doskey py36=C:\32\Python36\python.exe $*
+ set Path=C:\msys64\mingw32\bin;%Path%
---
+++ msys2/64bit.bat
@@ -0,0 +1,7 @@
+@echo off
+ doskey py26=C:\64\Python26\python.exe $*
+ doskey py27=C:\64\Python27\python.exe $*
+ doskey py34=C:\64\Python34\python.exe $*
+ doskey py35=C:\64\Python35\python.exe $*
+ doskey py36=C:\64\Python36\python.exe $*
+ set Path=C:\msys64\mingw64\bin;%Path%
