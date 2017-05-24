200870ea97c35f01bf12ae41b3223c10d9f909b8
Vincent Klein <vincent.klein@u-bordeaux.fr>
2017-05-24 11:49:14 +0200

    Add more cython test on checktypes functions

--- test_cython/test_cython.pyx
+++ test_cython/test_cython.pyx
@@ -18,7 +18,18 @@ mpz_set_si(MPZ(<MPZ_Object *> y), 2)
 z = x + y + 1  # python operation!
 assert z == 6
 
+# Checktypes functions with a MPZ_Object
 assert MPZ_Check(<PyObject *>x)
+assert not MPQ_Check(<PyObject *>x)
+assert not MPC_Check(<PyObject *>x)
+assert not MPFR_Check(<PyObject *>x)
+
+# Checktypes functions with a python integer
+i=5
+assert not MPZ_Check(<PyObject *>i)
+assert not MPQ_Check(<PyObject *>i)
+assert not MPC_Check(<PyObject *>i)
+assert not MPFR_Check(<PyObject *>i)
 
 x = <object> GMPy_MPQ_New(NULL)
 y = <object> GMPy_MPQ_New(NULL)
@@ -29,6 +40,8 @@ mpq_set_si(MPQ(<MPQ_Object *> y), -3, 7)
 z = x + y - 1 # python operation!
 assert 28 * z == -33
 
-assert MPQ_Check(<PyObject *>y)
-
-
+# Checktypes functions with a MPZ_Object
+assert not MPZ_Check(<PyObject *>x)
+assert MPQ_Check(<PyObject *>x)
+assert not MPC_Check(<PyObject *>x)
+assert not MPFR_Check(<PyObject *>x)
