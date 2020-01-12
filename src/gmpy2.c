155f0559abd30ac937fb11c6da0f59501fddcfe0
Case Van Horsen <casevh@gmail.com>
2020-01-11 21:32:32 -0800

    Update version to 2.1.0b4.

--- src/gmpy2.c
+++ src/gmpy2.c
@@ -395,6 +395,11 @@
  *    2.1.0b3
  *    Version bump only.
  *
+ *    2.1.04b
+ *    Fix comparisons with mpq and custom rational objects.
+ *    Fixes for some uncommon integer conversions scenarios.
+ *
+ *
  ************************************************************************
  *
  * Discussion on sizes of C integer types.
@@ -454,7 +459,7 @@
 
 /* The following global strings are used by gmpy_misc.c. */
 
-char gmpy_version[] = "2.1.0b3";
+char gmpy_version[] = "2.1.0b4";
 
 char gmpy_license[] = "\
 The GMPY2 source code is licensed under LGPL 3 or later. The supported \
@@ -862,7 +867,7 @@ static PyMethodDef Pygmpy_methods [] =
 };
 
 static char _gmpy_docs[] =
-"gmpy2 2.1.0b2 - General Multiple-precision arithmetic for Python\n"
+"gmpy2 2.1.0b4 - General Multiple-precision arithmetic for Python\n"
 "\n"
 "gmpy2 supports several multiple-precision libraries. Integer and\n"
 "rational arithmetic is provided by the GMP library. Real floating-\n"
