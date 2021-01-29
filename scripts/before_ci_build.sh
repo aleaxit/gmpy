70d17f9002da2b1657abcb223a8e191f90aaee2d
Case Van Horsen <casevh@gmail.com>
2021-01-29 14:30:47 -0800

    Test wheel builds; correct GMP build error.

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -3,7 +3,7 @@ if [ ! -f finish_before_ci_build ]; then
     yum install -y wget lzip
     wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
     tar -xvf gmp-6.2.0.tar.lz
-    cd gmp-6.2.0 && ./configure && make --enable-fat -j4 && make install && cd ../
+    cd gmp-6.2.0 && ./configure --enable-fat && make -j4 && make install && cd ../
     wget https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
     tar -xvf mpfr-4.1.0.tar.gz
     cd mpfr-4.0.2 && ./configure && make -j4 && make install && cd ../
