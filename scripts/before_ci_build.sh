fad6d08082615100fcde22e0a5383eaa5de597bd
Case Van Horsen <casevh@gmail.com>
2021-01-28 23:02:27 -0800

    Test wheel builds; fix yaml file; update library versions.

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -5,11 +5,11 @@ if [ ! -f finish_before_ci_build ]; then
     tar -xvf gmp-6.2.0.tar.lz
     cd gmp-6.2.0 && ./configure && make --enable-fat -j4 && make install && cd ../
     wget https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
-    tar -xvf mpfr-4.0.2.tar.gz
+    tar -xvf mpfr-4.1.0.tar.gz
     cd mpfr-4.0.2 && ./configure && make -j4 && make install && cd ../
     wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
     tar -xvf mpc-1.1.0.tar.gz
-    cd mpc-1.1.0 && ./configure && make -j4 && make install && cd ../
+    cd mpc-1.2.1 && ./configure && make -j4 && make install && cd ../
     pip install Cython
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install gmp mpfr libmpc
