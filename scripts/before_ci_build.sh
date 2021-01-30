0068b58fe992ebafe39979f8b464c9844ea58a7a
Case Van Horsen <casevh@gmail.com>
2021-01-29 22:21:17 -0800

    Test wheel builds; correct MPFR build error.

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -4,11 +4,11 @@ if [ ! -f finish_before_ci_build ]; then
     wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
     tar -xvf gmp-6.2.0.tar.lz
     cd gmp-6.2.0 && ./configure --enable-fat && make -j4 && make install && cd ../
-    wget https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
+    wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz
     tar -xvf mpfr-4.1.0.tar.gz
-    cd mpfr-4.0.2 && ./configure && make -j4 && make install && cd ../
-    wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
-    tar -xvf mpc-1.1.0.tar.gz
+    cd mpfr-4.1.0 && ./configure && make -j4 && make install && cd ../
+    wget https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
+    tar -xvf mpc-1.2.1.tar.gz
     cd mpc-1.2.1 && ./configure && make -j4 && make install && cd ../
     pip install Cython
   elif [[ "$OSTYPE" == "darwin"* ]]; then
