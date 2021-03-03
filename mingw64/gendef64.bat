827bddab0183a2031214c624d1308994a052950a
Case Van Horsen <casevh@gmail.com>
2021-03-02 21:47:56 -0800

    WIP: Updates to mingw64 build environment.

---
+++ mingw64/gendef64.bat
@@ -0,0 +1,23 @@
+cd C:\64\Python27\libs
+gendef c:\Windows\System32\python27.dll
+dlltool --dllname c:\Windows\System32\python27.dll --def python27.def --output-lib libpython27.a
+
+cd C:\64\Python35\libs
+gendef c:\64\Python35\python35.dll
+dlltool --dllname c:\Windows\System32\python35.dll --def python35.def --output-lib libpython35.a
+
+cd C:\64\Python36\libs
+gendef c:\64\Python36\python36.dll
+dlltool --dllname c:\Windows\System32\python36.dll --def python36.def --output-lib libpython36.a
+
+cd C:\64\Python37\libs
+gendef c:\64\Python37\python37.dll
+dlltool --dllname c:\Windows\System32\python37.dll --def python37.def --output-lib libpython37.a
+
+cd C:\64\Python38\libs
+gendef c:\64\Python38\python38.dll
+dlltool --dllname c:\Windows\System32\python38.dll --def python38.def --output-lib libpython38.a
+
+cd C:\64\Python39\libs
+gendef c:\64\Python39\python39.dll
+dlltool --dllname c:\Windows\System32\python39.dll --def python39.def --output-lib libpython39.a
