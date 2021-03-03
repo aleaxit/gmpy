ec5ad70138eaa6350f0c4a5088fd0a559b2ad67b
Case Van Horsen <casevh@gmail.com>
2021-03-02 21:46:28 -0800

    WIP: Updates to mingw64 build environment.

--- mingw64/64bit.bat
+++ mingw64/64bit.bat
@@ -1,8 +1,8 @@
 @echo off
- doskey py27=%HOMEDRIVE%%HOMEPATH%\64\Python27\python.exe $*
- doskey py35=%HOMEDRIVE%%HOMEPATH%\64\Python35\python.exe $*
- doskey py36=%HOMEDRIVE%%HOMEPATH%\64\Python36\python.exe $*
- doskey py37=%HOMEDRIVE%%HOMEPATH%\64\Python37\python.exe $*
- doskey py38=%HOMEDRIVE%%HOMEPATH%\64\Python38\python.exe $*
- doskey py39=%HOMEDRIVE%%HOMEPATH%\64\Python39\python.exe $*
- set Path=C:\msys64\mingw64\bin;%Path%
+ doskey py27=C:\64\Python27\python.exe $*
+ doskey py35=C:\64\Python35\python.exe $*
+ doskey py36=C:\64\Python36\python.exe $*
+ doskey py37=C:\64\Python37\python.exe $*
+ doskey py38=C:\64\Python38\python.exe $*
+ doskey py39=C:\64\Python39\python.exe $*
+ set Path=C:\mingw64\bin;%Path%
--- mingw64/msys2_64_static.sh
+++ mingw64/msys2_64_static.sh
@@ -1,40 +1,50 @@
+cd $HOME
+
 mkdir $HOME/64
 mkdir $HOME/64/src
 mkdir $HOME/64/static
-mkdir $HOME/64/shared
 
 cd $HOME/64/src
 
-wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
-tar xf gmp-6.2.0.tar.lz
+if [ ! -f gmp-6.2.1.tar.lz ]; then
+    wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz
+fi
+tar xf gmp-6.2.1.tar.lz
 
-wget https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.bz2
+if [ ! -f mpfr-4.1.0.tar.bz2 ]; then
+    wget https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.bz2
+fi
 tar xf mpfr-4.1.0.tar.bz2
 cd mpfr-4.1.0/
-wget https://www.mpfr.org/mpfr-current/allpatches
+#wget https://www.mpfr.org/mpfr-current/allpatches
 patch -N -Z -p1 < allpatches
 cd ..
 
-wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.2.0.tar.gz
+if [ ! -f mpc-1.2.0.tar.gz ]; then
+    wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.2.0.tar.gz
+fi
 tar xf mpc-1.2.0.tar.gz
 
-cd gmp-6.2.0/
-./configure --prefix=$HOME/64/static --enable-static --disable-shared --enable-fat --with-pic
-make
+cd gmp-6.2.1/
+make distclean
+./configure --build=x86_64-pc-mingw64 --host=x86_64-pc-mingw64 --prefix=$HOME/64/static --enable-static --disable-shared --enable-fat --with-pic
+make -j4
 make check
 make install
 cd ..
 
 cd mpfr-4.1.0/
-./configure --prefix=$HOME/64/static --enable-gmp-internals --enable-static --disable-shared --disable-decimal-float --disable-float128 --with-pic --with-gmp=$HOME/64/static
-make
+make distclean
+./configure --build=x86_64-pc-mingw64 --host=x86_64-pc-mingw64 --prefix=$HOME/64/static --enable-static --disable-shared --disable-decimal-float --disable-float128 --with-pic --with-gmp=$HOME/64/static
+make -j4
 make check
 make install
 cd ..
 
 cd mpc-1.2.0/
-./configure --prefix=$HOME/64/static --enable-static --disable-shared --with-pic --with-gmp=$HOME/64/static --with-mpfr=$HOME/64/static
-make
+make distclean
+./configure --build=x86_64-pc-mingw64 --host=x86_64-pc-mingw64 --prefix=$HOME/64/static --enable-static --disable-shared --with-pic --with-gmp=$HOME/64/static --with-mpfr=$HOME/64/static
+make -j4
 make check
 make install
 cd ..
