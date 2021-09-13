ad7fa45b6695fab7c11c3b2b2d21894b795db4ae
Isuru Fernando <isuruf@gmail.com>
2021-09-12 23:30:48 -0500

    set host for gmp to support older architectures

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -3,7 +3,11 @@ if [ ! -f finish_before_ci_build ]; then
     yum install -y wget lzip
     wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
     tar -xvf gmp-6.2.0.tar.lz
-    cd gmp-6.2.0 && ./configure --enable-fat && make -j4 && make install && cd ../
+    # need to set host to the oldest triple to avoid building binaries
+    # that use build machine micro-architecure. configfsf.guess is the one that
+    # comes with autotools which is micro-architecture agnostic.
+    # config.guess is a custom gmp script which knows about micro-architectures.
+    cd gmp-6.2.0 && ./configure --enable-fat --host=$(./configfsf.guess) && make -j4 && make install && cd ../
     wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz
     tar -xvf mpfr-4.1.0.tar.gz
     cd mpfr-4.1.0 && ./configure && make -j4 && make install && cd ../
