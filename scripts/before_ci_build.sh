d8ec21df6d0d86f4e26af8ef82077bca11a2b3ae
Case Van Horsen <casevh@gmail.com>
2021-09-13 21:47:41 -0700

    Test new CI build script.

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -1,19 +1,20 @@
 if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    yum install -y wget lzip
-    wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
-    tar -xvf gmp-6.2.0.tar.lz
+    # yum install -y wget lzip
+    # wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
+    # tar -xvf gmp-6.2.0.tar.lz
     # need to set host to the oldest triple to avoid building binaries
     # that use build machine micro-architecure. configfsf.guess is the one that
     # comes with autotools which is micro-architecture agnostic.
     # config.guess is a custom gmp script which knows about micro-architectures.
-    cd gmp-6.2.0 && ./configure --enable-fat --host=$(./configfsf.guess) && make -j4 && make install && cd ../
-    wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz
-    tar -xvf mpfr-4.1.0.tar.gz
-    cd mpfr-4.1.0 && ./configure && make -j4 && make install && cd ../
-    wget https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
-    tar -xvf mpc-1.2.1.tar.gz
-    cd mpc-1.2.1 && ./configure && make -j4 && make install && cd ../
+    # cd gmp-6.2.0 && ./configure --enable-fat --host=$(./configfsf.guess) && make -j4 && make install && cd ../
+    # wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz
+    # tar -xvf mpfr-4.1.0.tar.gz
+    # cd mpfr-4.1.0 && ./configure && make -j4 && make install && cd ../
+    # wget https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
+    # tar -xvf mpc-1.2.1.tar.gz
+    # cd mpc-1.2.1 && ./configure && make -j4 && make install && cd ../
+    yum install -y libmpc-dev
     pip install Cython
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install gmp mpfr libmpc
