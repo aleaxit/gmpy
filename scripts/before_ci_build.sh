4876945d1ff8424f1fe747da8b34d1c8777ccfef
KK <bebound@gmail.com>
2022-01-08 21:24:08 +0800

    Init

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -3,6 +3,9 @@ MPFR_VERSION=4.1.0
 MPC_VERSION=1.2.1
 if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" || "$OSTYPE" == "darwin"* ]]; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+      yum install -y xz
+    fi
     curl -O https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.xz
     tar -xvf gmp-${GMP_VERSION}.tar.xz
     # need to set host to the oldest triple to avoid building binaries
