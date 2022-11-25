6c1c1c2d5e46c50f409ded1082cb2b0563bd4dc0
Case Van Horsen <casevh@gmail.com>
2022-11-24 19:16:41 -0800

    Update MPFR version number and download location.

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -1,5 +1,5 @@
 GMP_VERSION=6.2.1
-MPFR_VERSION=4.1.0
+MPFR_VERSION=4.1.1
 MPC_VERSION=1.2.1
 if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" || "$OSTYPE" == "darwin"* ]]; then
@@ -13,7 +13,7 @@ if [ ! -f finish_before_ci_build ]; then
     # comes with autotools which is micro-architecture agnostic.
     # config.guess is a custom gmp script which knows about micro-architectures.
     cd gmp-${GMP_VERSION} && ./configure --enable-fat --host=$(./configfsf.guess) && make -j4 && make install && cd ../
-    curl -O -k https://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.gz
+    curl -O -k //ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.gz
     tar -xvf mpfr-${MPFR_VERSION}.tar.gz
     cd mpfr-${MPFR_VERSION} && ./configure && make -j4 && make install && cd ../
     curl -O https://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
