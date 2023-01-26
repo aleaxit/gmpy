3c952d0f342b6d21f72e22eaf63f5cd2ee7a8b72
Sergey B Kirpichev <skirpichev@gmail.com>
2023-01-26 10:54:23 +0300

    Simplify wheels building (linux and macos)

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -1,11 +1,10 @@
+set -e
+
 GMP_VERSION=6.2.1
 MPFR_VERSION=4.1.1
 MPC_VERSION=1.2.1
 if [ ! -f finish_before_ci_build ]; then
-  if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" || "$OSTYPE" == "darwin"* ]]; then
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-      yum install -y xz
-    fi
+  if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" ]]; then
     curl -O https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
     tar -xvf gmp-${GMP_VERSION}.tar.xz
     # need to set host to the oldest triple to avoid building binaries
