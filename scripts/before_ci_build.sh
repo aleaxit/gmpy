d1d448ffd2336c9f8407e812fbc9942fb22e0876
Sergey B Kirpichev <skirpichev@gmail.com>
2023-01-28 04:57:04 +0300

    Revert ad7fa45
    It was introduced for #305 but actually #307 (1st commit) was a
    solution.

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -7,13 +7,9 @@ if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" || "$OSTYPE" == "darwin"* ]]; then
     curl -O https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
     tar -xvf gmp-${GMP_VERSION}.tar.xz
-    # need to set host to the oldest triple to avoid building binaries
-    # that use build machine micro-architecure. configfsf.guess is the one that
-    # comes with autotools which is micro-architecture agnostic.
-    # config.guess is a custom gmp script which knows about micro-architectures.
     cd gmp-${GMP_VERSION}
     patch -N -Z -p0 < ../scripts/patch-arm64.diff && cd ..
-    cd gmp-${GMP_VERSION} && ./configure --enable-fat --host=$(./configfsf.guess) && make -j4 && make install && cd ../
+    cd gmp-${GMP_VERSION} && ./configure --enable-fat && make -j4 && make install && cd ../
     curl -O -k https://ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.gz
     tar -xvf mpfr-${MPFR_VERSION}.tar.gz
     cd mpfr-${MPFR_VERSION} && ./configure && make -j4 && make install && cd ../
