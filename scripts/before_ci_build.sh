b79f8073eee79dade706c0b6ae00cb768ba53b82
Sergey B Kirpichev <skirpichev@gmail.com>
2023-01-29 07:40:41 +0300

    XXX debug

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -5,16 +5,18 @@ MPFR_VERSION=4.1.1
 MPC_VERSION=1.2.1
 if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" || "$OSTYPE" == "darwin"* ]]; then
-    curl -O https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
-    tar -xvf gmp-${GMP_VERSION}.tar.xz
+	echo "Run scripts/before_ci_build.sh on $OSTYPE"
+    set
+    curl -s -O https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
+    tar -xf gmp-${GMP_VERSION}.tar.xz
     cd gmp-${GMP_VERSION}
     patch -N -Z -p0 < ../scripts/patch-arm64.diff && cd ..
     cd gmp-${GMP_VERSION} && ./configure --enable-fat && make -j4 && make install && cd ../
-    curl -O -k https://ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.gz
-    tar -xvf mpfr-${MPFR_VERSION}.tar.gz
+    curl -s -O https://ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.gz
+    tar -xf mpfr-${MPFR_VERSION}.tar.gz
     cd mpfr-${MPFR_VERSION} && ./configure && make -j4 && make install && cd ../
-    curl -O https://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
-    tar -xvf mpc-${MPC_VERSION}.tar.gz
+    curl -s -O https://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
+    tar -xf mpc-${MPC_VERSION}.tar.gz
     cd mpc-${MPC_VERSION} && ./configure && make -j4 && make install && cd ../
     pip install Cython
   fi
