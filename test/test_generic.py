4ab616fe3000c5f6adf1e933c9fea27cfcd65ccb
Sergey B Kirpichev <skirpichev@gmail.com>
2023-05-02 11:16:04 +0300

    Port test/test_gmpy2_plus.txt to pytest framework

--- test/test_generic.py
+++ test/test_generic.py
@@ -20,3 +20,21 @@ def test_minus():
 
     pytest.raises(TypeError, lambda: ctx.minus('invalid'))
     pytest.raises(TypeError, lambda: ctx.minus())
+
+
+def test_plus():
+    ctx = get_context()
+    assert ctx.plus(5) == mpz(5)
+    assert ctx.plus(-5) == mpz(-5)
+    assert ctx.plus(Fraction(4,5)) == mpq(4,5)
+    assert ctx.plus(4.5) == mpfr('4.5')
+    assert ctx.plus(complex(5.2,5)) == mpc('5.2000000000000002+5.0j')
+    assert ctx.plus(mpz(421)) == mpz(421)
+
+    pytest.raises(TypeError, lambda: ctx.plus('invalid'))
+    pytest.raises(TypeError, lambda: ctx.plus())
+
+    assert + mpz(421) == mpz(421)
+    assert + mpq('4/5') == mpq(4,5)
+    assert + mpfr('inf') == mpfr('inf')
+    assert + mpc(65.0, 45) == mpc('65.0+45.0j')
