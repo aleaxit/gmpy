cc85baaf22bb3a50bb30e4ce541286adbdd3e359
Sergey B Kirpichev <skirpichev@gmail.com>
2023-10-09 09:01:11 +0300

    Port test/test_gmpy2_xmpz_limbs.txt to pytest framework

--- test/test_xmpz.py
+++ test/test_xmpz.py
@@ -1,3 +1,5 @@
+from ctypes import memmove
+
 import pytest
 
 from gmpy2 import xmpz
@@ -13,3 +15,35 @@ def test_xmpz_digits():
 
     pytest.raises(ValueError, lambda: x.digits(0))
     pytest.raises(ValueError, lambda: x.digits(63))
+
+
+def test_xmpz_limbs():
+    x, y = xmpz(123456789), xmpz(0);
+    x_limbs, num_limbs = x.limbs_read(), x.num_limbs()
+
+    assert x_limbs != 0
+    assert num_limbs > 0
+
+    y_limbs = y.limbs_write(num_limbs)
+
+    assert y_limbs != 0
+    assert memmove(y_limbs, x_limbs, num_limbs * xmpz.limb_size) > 0
+
+    y.limbs_finish(num_limbs)
+
+    assert int(y) == 123456789
+
+    x, y = xmpz(987654321), xmpz(0)
+    x_limbs, num_limbs = x.limbs_read(), x.num_limbs()
+
+    assert x_limbs != 0
+    assert num_limbs > 0
+
+    y_limbs = y.limbs_modify(num_limbs);
+
+    assert y_limbs != 0
+    assert memmove(y_limbs, x_limbs, num_limbs * xmpz.limb_size) > 0
+
+    y.limbs_finish(num_limbs)
+
+    assert int(y) == 987654321
