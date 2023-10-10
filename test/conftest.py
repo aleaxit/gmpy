7a979793b63990d250882125f8481e42a4abb877
Sergey B Kirpichev <skirpichev@gmail.com>
2023-10-10 06:49:37 +0300

    Ignore *.txt files for pytest's test collection

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,9 @@ import pytest
 import gmpy2
 
 
+collect_ignore_glob = ['*.txt']
+
+
 @pytest.fixture(autouse=True, scope='module')
 def _set_default_context():
     gmpy2.set_context(gmpy2.context())
