ac2cfe2b43b4926922f5c4a0023c8140f8c956ea
Sergey B Kirpichev <skirpichev@gmail.com>
2023-10-30 08:27:24 +0300

    Add pytest_report_header(), like test/runtests.py

--- test/conftest.py
+++ test/conftest.py
@@ -9,3 +9,13 @@ collect_ignore_glob = ['*.txt']
 @pytest.fixture(autouse=True, scope='function')
 def _set_default_context():
     gmpy2.set_context(gmpy2.context())
+
+
+def pytest_report_header(config):
+    print("""
+  Mutliple-precision library:     {0}
+  Floating-point library:         {1}
+  Complex library:                {2}
+""".format(gmpy2.mp_version(),
+           gmpy2.mpfr_version(),
+           gmpy2.mpc_version()))
