f30c1d6f474eb9bbdfac7f554115c2ff0aafd15f
Isuru Fernando <isuruf@gmail.com>
2023-10-29 22:08:22 -0500

    Fix microarchitecture specific code in common code paths

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -7,7 +7,12 @@ if [ ! -f finish_before_ci_build ]; then
   if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-musl" || "$OSTYPE" == "darwin"* ]]; then
     curl -s -O https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
     tar -xf gmp-${GMP_VERSION}.tar.xz
-    cd gmp-${GMP_VERSION} && ./configure --enable-fat && make -j4 && make install && cd ../
+    cd gmp-${GMP_VERSION}
+    # config.guess uses microarchitecture and configfsf.guess doesn't
+    # We replace config.guess with configfsf.guess to avoid microarchitecture
+    # specific code in common code.
+    rm config.guess && mv configfsf.guess config.guess && chmod +x config.guess
+    ./configure --enable-fat && make -j4 && make install && cd ../
     curl -s -O https://ftp.gnu.org/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.gz
     tar -xf mpfr-${MPFR_VERSION}.tar.gz
     cd mpfr-${MPFR_VERSION} && ./configure && make -j4 && make install && cd ../
