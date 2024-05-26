26cea637913bcbfc2a801d8c6336e077e699fa06
Case Van Horsen <casevh@gmail.com>
2024-05-26 11:35:54 -0700

    Bump version to rc1.

--- src/gmpy2.c
+++ src/gmpy2.c
@@ -118,7 +118,7 @@
 
 /* The following global strings are used by gmpy_misc.c. */
 
-char gmpy_version[] = "2.2.0b1";
+char gmpy_version[] = "2.2.0rc1";
 
 char gmpy_license[] = "\
 The GMPY2 source code is licensed under LGPL 3 or later. The supported \
